8ac2dffa3bc0eabe2e1a5b962c12ef92a30de0a5
Andrew Preciado <ap90528n@pace.edu>
2016-10-05 17:02:52 -0400

    Add TwilioSend directory

---
+++ TwilioSend/index.php
@@ -0,0 +1,17 @@
+<!--index php file-->
+<?php
+
+//echo "php on heroku is awesome";
+
+ // this line loads the library 
+require('/path/to/twilio-php/Services/Twilio.php'); 
+ 
+$account_sid = 'AC22436d8a2fc6741ccbc242414afece1e'; 
+$auth_token = '82910f44bb1146714ab0f32bbe475662'; 
+$client = new Services_Twilio($account_sid, $auth_token); 
+ 
+$client->account->messages->create(array( 
+	'To' => "1 929 216 8151", 
+	'From' => "+12014313256", 
+	'Body' => "Hello from Andrew Preciado",   
+)); 
