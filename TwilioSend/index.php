0848e48430c835335ce69333bf9cd7e47160c8d6
Andrew Preciado <ap90528n@pace.edu>
2016-10-12 00:12:03 -0400

    new twiliosend

--- TwilioSend/index.php
+++ TwilioSend/index.php
@@ -1,18 +1,33 @@
 <!--index php file-->
 <?php
 
-//header( 'CS6432016/TwilioSend/index.html' ) ;
-echo "php on heroku is awesome";
+// ==== Control Vars =======
+$strFromNumber = "+12014313256";
+$strToNumber = "+19292168151";
+$strMsg = "Hello from Andrew Preciado!";  
+$aryResponse = array();
 
- // this line loads the library 
-require('/path/to/twilio-php/Services/Twilio.php'); 
- 
-$account_sid = 'AC22436d8a2fc6741ccbc242414afece1e'; 
-$auth_token = '82910f44bb1146714ab0f32bbe475662'; 
-$client = new Services_Twilio($account_sid, $auth_token); 
- 
-$client->account->messages->create(array( 
-	'To' => "1 929 216 8151", 
-	'From' => "+12014313256", 
-	'Body' => "Hello from Andrew Preciado",   
-)); 
+// include the Twilio PHP library - download from 
+    // http://www.twilio.com/docs/libraries/
+    require_once ("inc/Services/Twilio.php");
+
+// set our AccountSid and AuthToken - from www.twilio.com/user/account
+    $AccountSid = "AC22436d8a2fc6741ccbc242414afece1e";
+    $AuthToken = "82910f44bb1146714ab0f32bbe475662";
+
+// ini a new Twilio Rest Client
+    $objConnection = new Services_Twilio($AccountSid, $AuthToken);
+
+// Send a new outgoinging SMS by POSTing to the SMS resource */
+    $bSuccess = $objConnection->account->sms_messages->create(
+        
+        $strFromNumber, 	// number we are sending From 
+        $strToNumber,           // number we are sending To
+        $strMsg			// the sms body
+    );
+
+	$aryResponse["SentMsg"] = $strMsg;
+    $aryResponse["Success"] = true;
+    
+    
+    echo json_encode($aryResponse);
